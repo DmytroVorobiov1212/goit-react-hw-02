4ac5387e783e2c393681d796ced0b92e3acb9e21
DemonVenom666 <demonvenom9@gmail.com>
2025-03-28 11:01:31 +0100

    finaly

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,7 +1,9 @@
+import s from './Notification.module.css';
+
 const Notification = () => {
   return (
     <>
-      <p>No feedback yet</p>
+      <p className={s.text}>No feedback yet</p>
     </>
   );
 };
